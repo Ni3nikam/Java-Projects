e95bde9a9d437520c35491b71f51c129e5a1ae26
admin <admin@DESKTOP-0FB4GQV>
2023-08-25 15:20:41 +0530

    version 1 testing commit

---
+++ Nitin Event Manegement Services/src/main/java/com/NitinEMSApplication/NitinEMSApplication.java
@@ -0,0 +1,14 @@
+package com.NitinEMSApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitinEMSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NitinEMSApplication.class, args);
+		System.err.println("Nitin Event Management Services is Runnnnnnnnning.............");
+	}
+
+}
